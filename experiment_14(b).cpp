606f771e5c1aeb10ba006aeadc84398487e3c682
navya <navyapkhajuria@gmail.com>
2025-09-12 16:23:42 +0530

    Create experiment_14(b).cpp

---
+++ experiment_14(b).cpp
@@ -0,0 +1,50 @@
+//Navya Pawan Khajuria 
+//24070123151
+//B3
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Brand {
+protected:
+    string brand;
+public:
+    Brand() {
+        brand = "Mercedes-Benz";
+    }
+};
+
+class Features {
+public:
+    void classic() {
+        cout << "Features: Luxury" << endl;
+    }
+};
+
+class Carr : public Brand, public Features {
+    string model;
+public:
+    Carr() {
+        cout << "Enter Model: ";
+        cin >> model;
+    }
+
+    void display() {
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+    }
+};
+
+int main() {
+    Carr c;
+    c.display();
+    c.classic();
+    return 0;
+}
+
+//OUTPUT:
+//Enter Model: S-Class 
+//Brand: Mercedes-Benz
+//Model: S-Class
+//Features: Luxury
