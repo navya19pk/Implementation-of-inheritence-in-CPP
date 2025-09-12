4f0a9bc345d83fa976a72b8d26f1e5d5933b8668
navya <navyapkhajuria@gmail.com>
2025-09-12 16:22:32 +0530

    Create experiment_14(a).cpp

---
+++ experiment_14(a).cpp
@@ -0,0 +1,31 @@
+//Navya Pawan Khajuria
+//24070123151
+//B3
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class car {
+    public:
+    string a ="skoda ";
+    void model(){
+        cout<<"SUV"<<endl;
+    }
+};
+class name: public car {
+    public:
+    string b ="octavia";
+};
+
+int main(){
+    name u1;
+    u1.model();
+    cout<<u1.a ;
+    cout<< u1.b;
+}
+
+
+// OUTPUT:
+//SUV
+//skoda octavia
